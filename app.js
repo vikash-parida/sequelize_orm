f046a351d67ccee0bd1901acb2fd3f569f65ff1f
vikash067 <vikash@nimapinfotech.com>
2022-06-15 16:26:23 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const express = require('express');
+const app = express();
+const port = process.env.PORT ||8000; ;
+
+app.listen(port,()=>{
+    console.log('listening on port:'+ port);
+});
