15dd77ce37fa6239e688768ee88423a5b4c6e40c
vikash067 <vikash@nimapinfotech.com>
2022-06-16 11:51:06 +0530

    create user models and sync data to the database

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+const models = require('./models')
 const express = require('express');
 const app = express();
-const port = process.env.PORT ||8000; ;
+
+const port = process.env.PORT ||8000; 
+
+
+ models.sequelize.authenticate().then(()=>{
+    console.log(' Database is synced successfully')
+ }).catch(err=>{
+    console.log(err)
+ })
+app.use('/',(req, res, )=>{
+   res.send("welcome");
+})
 
 app.listen(port,()=>{
     console.log('listening on port:'+ port);
